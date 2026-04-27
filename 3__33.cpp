d253f25efda753843ddee54a35138ec2c454add6
YaroslavPelekhov <psgpele1729@mail.ru>
2026-04-27 09:13:38 +0300

    Create 3__33.cpp

---
+++ 3__33.cpp
@@ -0,0 +1,74 @@
+
+
+
+
+#include <iostream>
+#include <string>
+#include <map>
+#include <set>
+
+bool make_step(const std::map<std::pair<std::string, char>, std::string>& transitions,
+               std::string& state, char cur_char) {
+    auto trans_it = transitions.find({state, cur_char});
+    if (trans_it == transitions.end()) {
+        return false;
+    }
+    state = trans_it->second;
+    return true;
+}
+
+int main() {
+    std::map<std::pair<std::string, char>, std::string> transitions;
+    std::set<std::string> accepting_states;
+
+    std::string cur_state, sym_token, new_state;
+    while (std::cin >> cur_state) {
+        if (cur_state == "END") {
+            break;
+        }
+        std::cin >> sym_token >> new_state;
+        char sym_char = sym_token[0];
+        transitions[{cur_state, sym_char}] = new_state;
+    }
+
+    std::string accept_state;
+    while (std::cin >> accept_state) {
+        if (accept_state == "END") {
+            break;
+        }
+        accepting_states.insert(accept_state);
+    }
+
+    std::string start_state, input_word;
+    std::cin >> start_state >> input_word;
+
+    std::string state = start_state;
+    size_t read_count = 0;
+    bool dead_end = false;
+
+    for (size_t pos = 0; pos < input_word.size(); pos++) {
+        bool step_ok = make_step(transitions, state, input_word[pos]);
+        if (!step_ok) {
+            dead_end = true;
+            break;
+        }
+        read_count++;
+    }
+
+    bool is_accept = accepting_states.count(state) > 0;
+
+    if (dead_end) {
+        std::cout << 0 << "\n";
+        std::cout << read_count << "\n";
+    } else {
+        if (is_accept) {
+            std::cout << 1 << "\n";
+        } else {
+            std::cout << 0 << "\n";
+        }
+        std::cout << read_count + 1 << "\n";
+    }
+    std::cout << state << "\n";
+
+    return 0;
+}
